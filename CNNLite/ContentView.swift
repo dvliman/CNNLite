b8053db3f46dd7a66eae6c8b2b5a176ae976545c
David Liman <limanoit@gmail.com>
2019-12-27 01:45:25 -0800

    Initial Commit

---
+++ CNNLite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CNNLite
+//
+//  Created by dv on 12/27/19.
+//  Copyright © 2019 David Liman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
